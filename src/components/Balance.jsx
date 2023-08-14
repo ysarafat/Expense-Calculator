67947659ec0d4c085ce614a125174182738ea5b7
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-08-14 22:35:50 +0600

    calculate balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Balance() {
+    const { transactions } = useSelector((state) => state.transaction);
+
+    const calculateBalance = (transactions) => {
+        let balance = 0;
+        transactions.forEach((transaction) => {
+            const { type, amount } = transaction;
+            if (type === 'income') {
+                balance += amount;
+            } else {
+                balance -= amount;
+            }
+        });
+        return balance;
+    };
     return (
         <div className="top_card">
             <p>Your Current Balance</p>
             <h3>
-                <span>৳</span>
-                <span>10500</span>
+                <span>৳</span>{' '}
+                <span>{transactions.length > 0 ? calculateBalance(transactions) : 0}</span>
             </h3>
         </div>
     );
